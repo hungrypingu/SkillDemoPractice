b84c25457dfe25023c4cf63dd683aa8a3418cbed
hungrypingu <jjk015@ucsd.edu>
2022-02-04 13:43:59 -0800

    Update demoPracticeTest.java

--- demoPracticeTest.java
+++ demoPracticeTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class demoPracticeTest {
     @Test
     public void determinant() {
-        assertEquals(demoPractice.determinant(2, 3, 4, 5), -2);
+        assertEquals(demoPractice.determinant(2, 3, 4, 5), -1); //fails unless changed to -2
     }
 }
